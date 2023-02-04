b1e77378da3f8ef65b70e295214bc5454490edb7
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-05 00:09:59 +0100

    Component forecast weather data.
    Receives weather data as props from parent.

---
+++ src/components/Forecast.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { WeatherData } from '../types/types';
+
+const props = defineProps<{ apiData: WeatherData | null; metric: boolean }>();
+</script>
+<template>
+	<div class="forecast">
+		<div v-if="apiData">
+			sunrise:{{ apiData.sys.sunrise }} sunset:{{ apiData.sys.sunset }}
+		</div>
+	</div>
+</template>
+<style scoped>
+.forecast {
+	grid-column: 1/3;
+}
+</style>
