f16746f06a8dc8bc94d932eac0dd63ced2a222d9
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-05 00:04:29 +0100

    Adding Controls component.
    Responsible for handling location search input and emitting
    weather search event

---
+++ src/components/Controls.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const props = defineProps({
+	metric: Boolean,
+});
+const emit = defineEmits(['changeUnits', 'searchWeather']);
+const searchValue = ref('');
+</script>
+<template>
+	<div class="controls">
+		<input
+			type="text"
+			id="search"
+			name="search"
+			v-model="searchValue"
+			placeholder="Search location..."
+			@keypress.enter="emit('searchWeather', searchValue)"
+		/>
+		<button type="button" @click="emit('changeUnits')">
+			Use {{ metric ? 'imperial' : 'metric' }}
+		</button>
+	</div>
+</template>
+<style scoped>
+.controls {
+	grid-column: 1/3;
+}
+</style>
