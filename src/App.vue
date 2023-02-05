35eef829ec44f4e4b0f640f9118cc568a6951c2d
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-05 23:16:56 +0100

    Fetching arbitrary weather data while waiting
    for user to accept/decline request for geolocation.
    Adding watcher to fetch new weather data if user accepts.
    Adding state tracking fetching status.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import Current from './components/Current.vue';
 import Extra from './components/Extra.vue';
 import Forecast from './components/Forecast.vue';
@@ -8,56 +8,92 @@ import axios from 'axios';
 import type { AxiosResponse } from 'axios';
 import type { GeocodingData, WeatherData } from './types/types';
 
-const metric = ref(true);
+const metric = ref<Boolean>(true);
+const geolocation = ref<{ latitude: number, longitude: number }>({ latitude: 52.374, longitude: 4.8897 });
 const apiData = ref<WeatherData | null>(null);
+const fetching = ref<Boolean>(true);
 
 onMounted(async () => {
 	try {
+		getGeolocation()
 		const res: AxiosResponse<WeatherData> = await axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?lat=52.374&lon=4.8897&&appid=${
-				import.meta.env.API_KEY
-			}`
+			`https://api.openweathermap.org/data/2.5/weather?lat=52.374&lon=4.8897&&appid=${import.meta.env.VITE_API_KEY}`
 		);
 		apiData.value = res.data;
+		fetching.value = false
 	} catch (error) {
 		console.error(error);
+		fetching.value = false
 	}
 });
 
+watch(geolocation, async () => {
+	fetching.value = true
+	const res: AxiosResponse<WeatherData> = await axios.get(
+		`https://api.openweathermap.org/data/2.5/weather?lat=${geolocation.value.latitude}&lon=${geolocation.value.longitude}&appid=${import.meta.env.VITE_API_KEY}`
+	);
+	apiData.value = res.data;
+	fetching.value = false
+})
+
+
 function changeUnits() {
 	metric.value = !metric.value;
+}
+
+function getGeolocation(): GeolocationPosition | null {
+	fetching.value = true
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((location) => {
+			const { latitude, longitude } = location.coords
+			geolocation.value = { latitude, longitude }
+		}, handleGeolocationError);
+	} else {
+		console.log('Geolocation is not supported by this browser.');
+	}
+	fetching.value = false
+	return null;
+}
+
+function handleGeolocationError(error: GeolocationPositionError) {
+	switch (error.code) {
+		case error.PERMISSION_DENIED:
+			console.log('User denied the request for geolocation.');
+			break;
+		case error.POSITION_UNAVAILABLE:
+			console.log('Geolocation information is unavailable.');
+			break;
+		case error.TIMEOUT:
+			console.log('The request to get user geolocation timed out.');
+			break;
+		default:
+			console.log('An unknown geolocation error occurred.');
+			break;
+	}
 }
 
 async function searchWeather(query: string) {
 	try {
 		if (!query) return;
 		const resGeocoding: AxiosResponse<GeocodingData[]> = await axios.get(
-			`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1appid=${
-				import.meta.env.API_KEY
-			}`
+			`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1appid=${import.meta.env.VITE_API_KEY}`
 		);
 		const resWeather: AxiosResponse<WeatherData> = await axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?lat=${
-				resGeocoding.data[0].lat
-			}&lon=${resGeocoding.data[0].lon}&&appid=${import.meta.env.API_KEY}`
+			`https://api.openweathermap.org/data/2.5/weather?lat=${resGeocoding.data[0].lat}&lon=${resGeocoding.data[0].lon}&&appid=${import.meta.env.VITE_API_KEY}`
 		);
 		apiData.value = resWeather.data;
 	} catch (error) {
 		console.error(error);
 	}
 }
-</script>
 
+</script>
 <template>
 	<main>
-		<Controls
-			@changeUnits="changeUnits"
-			@searchWeather="searchWeather"
-			:metric="metric"
-		/>
-		<Current :apiData="apiData" :metric="metric" />
-		<Extra :apiData="apiData" :metric="metric" />
-		<Forecast :apiData="apiData" :metric="metric" />
+		<Controls @changeUnits="changeUnits" @searchWeather="searchWeather" :metric="metric" />
+		<Current :apiData="apiData" :metric="metric" :fetching="fetching" />
+		<Extra :apiData="apiData" :metric="metric" :fetching="fetching" />
+		<Forecast :apiData="apiData" :metric="metric" :fetching="fetching" />
 	</main>
 </template>
 
