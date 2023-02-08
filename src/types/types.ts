6eb9f26e2992cf73587bfc7eebc7b55c540cb9ec
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-08 23:08:33 +0100

    Updating types.

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,42 +1,90 @@
 export interface GeocodingData {
-  name: string;
-  lat: string;
-  lon: string;
-  country: string;
-  state?: string;
+	name: string;
+	lat: number;
+	lon: number;
+	country: string;
+	state: string;
 }
 [];
 
-export interface WeatherData {
-  coord: { lat: number; lon: number };
-  weather: {
-    id: number;
-    main: string;
-    description: string;
-    icon: string;
-  };
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-    sea_level: number;
-    grnd_level: number;
-  };
-  visibility: number;
-  wind: { speed: number; deg: number; gust?: number };
-  rain?: { "1h": number; "3h": number };
-  clouds: { all: number };
-  snow?: { "1h": number; "3h": number };
-  dt: number;
-  sys: {
-    country: string;
-    sunrise: number;
-    sunset: number;
-  };
-  timezone: number;
-  id: number;
-  name: string;
+export interface OneCallWeatherData {
+	lat: number;
+	lon: number;
+	timezone: string;
+	timezone_offset: number;
+	current: {
+		dt: number;
+		sunrise: number;
+		sunset: number;
+		temp: number;
+		feels_like: number;
+		pressure: number;
+		humidity: number;
+		dew_point: number;
+		uvi: number;
+		clouds: number;
+		visibility: number;
+		wind_speed: number;
+		wind_deg: number;
+		wind_gust?: number;
+		rain?: { '1h': number };
+		snow?: { '1h': number };
+		weather: { id: number; main: string; description: string; icon: string }[];
+	};
+	daily: {
+		clouds: number;
+		dew_point: number;
+		dt: number;
+		feels_like: {
+			day: number;
+			eve: number;
+			morn: number;
+			night: number;
+		};
+		humidity: number;
+		moon_phase: number;
+		moonrise: number;
+		moonset: number;
+		pop: number;
+		pressure: number;
+		sunrise: number;
+		sunset: number;
+		temp: {
+			day: number;
+			eve: number;
+			max: number;
+			min: number;
+			morn: number;
+			night: number;
+		};
+		uvi: number;
+		weather: { id: number; main: string; description: string; icon: string }[];
+		wind_deg: number;
+		wind_gust?: number;
+		wind_speed: number;
+	}[];
+	hourly: {
+		clouds: number;
+		dew_point: number;
+		dt: number;
+		feels_like: number;
+		humidity: number;
+		pop: number;
+		pressure: number;
+		temp: number;
+		uvi: number;
+		visibility: number;
+		weather: { id: number; main: string; description: string; icon: string }[];
+		wind_deg: number;
+		wind_gust?: number;
+		wind_speed: number;
+	}[];
+	alerts?: {
+		sender_name: string;
+		event: string;
+		start: number;
+		end: number;
+		description: string;
+		tags: string[];
+	};
 }
