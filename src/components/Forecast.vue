9d38ead49d140f3f83d1c90bcf35546bdf5cc036
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-08 23:31:53 +0100

    Updating props types.
    Adding state handling which forecast data to display and necessary
    function to toggle it
    Adding buttons for toggling state.
    Rendering appropriate forecast data.
    Adding minor styling.

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,17 +1,82 @@
 <script setup lang="ts">
-import type { WeatherData } from '../types/types';
+import { ref } from "vue";
+import type { OneCallWeatherData } from "../types/types";
+import { convertTemp, convertSpeed } from '../utilities/converters'
 
-const props = defineProps<{ apiData: WeatherData | null; metric: boolean }>();
+const props = defineProps<{
+	data: OneCallWeatherData | null;
+	metric: boolean;
+	fetching: boolean;
+}>();
+
+const displayedForecast = ref<"daily" | "hourly">("daily");
+
+function changeForecast(value: "daily" | "hourly") {
+	displayedForecast.value = value
+}
+
+
 </script>
 <template>
-	<div class="forecast">
-		<div v-if="apiData">
-			sunrise:{{ apiData.sys.sunrise }} sunset:{{ apiData.sys.sunset }}
+	<div v-if="!fetching && data" class="weather-forecast">
+		<div class="forecast-controls">
+			<div>
+				<button type="button" @click="changeForecast('daily')">Daily</button>
+				<button type="button" @click="changeForecast('hourly')">Hourly</button>
+			</div>
+
+		</div>
+		<div class="forecast-display">
+			<div v-if="displayedForecast === 'daily'" class="forecast-daily">
+				<div v-for="(d, i) in data[displayedForecast]" :key="i">
+					<div>{{
+						new Date(d.dt * 1000).toLocaleString(undefined,
+							{
+								weekday: "long",
+								month: 'numeric',
+								day: 'numeric',
+								year: 'numeric'
+							})
+					}}</div>
+					<img :src="`https://openweathermap.org/img/wn/${d.weather[0].icon}.png`" :alt="d.weather[0].main" />
+					<div>{{ d.weather[0].main }}</div>
+					<div>{{ convertTemp(metric, d.temp.day) }}</div>
+				</div>
+			</div>
+			<div v-else-if="displayedForecast === 'hourly'" class="forecast-hourly">
+				<div v-for="(d, i) in data.hourly" :key="i">
+					<div>{{
+						new Date(d.dt * 1000).toLocaleString(undefined,
+							{
+								weekday: "long",
+								month: 'numeric',
+								day: 'numeric',
+								year: 'numeric'
+							})
+					}}</div>
+					<img :src="`https://openweathermap.org/img/wn/${d.weather[0].icon}.png`" :alt="d.weather[0].main" />
+					<div>{{ d.weather[0].main }}</div>
+					<div>{{ convertTemp(metric, d.temp) }}</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
-<style scoped>
-.forecast {
-	grid-column: 1/3;
+<style scoped lang="scss">
+.weather-forecast {
+	margin-top: auto;
+}
+
+.forecast-controls {
+	display: flex;
+	justify-content: space-between;
+}
+
+.forecast-daily,
+.forecast-hourly {
+	display: flex;
+	gap: 2rem;
+	padding: 2rem 0;
+	overflow: scroll
 }
 </style>
