a8d1e7340a5b9384685e139f8ce046c775ba81a1
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-08 23:27:08 +0100

    Moving fetching function to separate file.
    Importing said functions/
    Updating initial state.
    Updating requestng geoloation from user. On success fetching weather data
    from api, using that data to then fetch more data on user's geolocation.
    Minor layout changes.
    Removing grid styling, replaing with flex.

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,119 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
-import Current from './components/Current.vue';
-import Extra from './components/Extra.vue';
-import Forecast from './components/Forecast.vue';
-import Controls from './components/Controls.vue';
-import axios from 'axios';
-import type { AxiosResponse } from 'axios';
-import type { GeocodingData, WeatherData } from './types/types';
+import { onMounted, ref, watch } from "vue";
+import Current from "./components/Current.vue";
+import Extra from "./components/Extra.vue";
+import Forecast from "./components/Forecast.vue";
+import Controls from "./components/Controls.vue";
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+import type { GeocodingData, OneCallWeatherData } from "./types/types";
+import { fetchGeolocation, fetchWeatherOneCall } from "./utilities/fetchWeather";
 
-const metric = ref<Boolean>(true);
-const geolocation = ref<{ latitude: number, longitude: number }>({ latitude: 52.374, longitude: 4.8897 });
-const apiData = ref<WeatherData | null>(null);
-const fetching = ref<Boolean>(true);
+const metric = ref<boolean>(true);
+const location = ref<GeocodingData | null>({
+	lat: 52.3727598,
+	lon: 4.8936041,
+	country: "NL",
+	name: "Amsterdam",
+	state: "North Holland",
+});
+const apiData = ref<OneCallWeatherData | null>(null);
+const fetching = ref<boolean>(true);
 
 onMounted(async () => {
 	try {
-		getGeolocation()
-		const res: AxiosResponse<WeatherData> = await axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?lat=52.374&lon=4.8897&&appid=${import.meta.env.VITE_API_KEY}`
-		);
-		apiData.value = res.data;
-		fetching.value = false
+		if (!location.value) return
+		fetching.value = true;
+		getGeolocation();
+		const { lat, lon } = location.value;
+		apiData.value = await fetchWeatherOneCall(lat, lon);
+		fetching.value = false;
 	} catch (error) {
 		console.error(error);
-		fetching.value = false
+		fetching.value = false;
 	}
 });
 
-watch(geolocation, async () => {
-	fetching.value = true
-	const res: AxiosResponse<WeatherData> = await axios.get(
-		`https://api.openweathermap.org/data/2.5/weather?lat=${geolocation.value.latitude}&lon=${geolocation.value.longitude}&appid=${import.meta.env.VITE_API_KEY}`
-	);
-	apiData.value = res.data;
-	fetching.value = false
-})
+watch(location, async () => {
+	if (!location.value) return
+	fetching.value = true;
+	const { lat, lon } = location.value;
+	apiData.value = await fetchWeatherOneCall(lat, lon);
+	fetching.value = false;
+});
 
+async function changeLocation(query: string) {
+	try {
+		if (!query) return;
+		location.value = await fetchGeolocation(query)
+	} catch (error) {
+		console.error(error);
+	}
+}
 
 function changeUnits() {
 	metric.value = !metric.value;
 }
 
 function getGeolocation(): GeolocationPosition | null {
-	fetching.value = true
+	fetching.value = true;
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition((location) => {
-			const { latitude, longitude } = location.coords
-			geolocation.value = { latitude, longitude }
-		}, handleGeolocationError);
+		navigator.geolocation.getCurrentPosition(async (position) => {
+			const { latitude, longitude } = position.coords;
+			const weather = await fetchWeatherOneCall(latitude, longitude);
+			if (!weather) throw new Error();
+			const query = weather.timezone.split("/")[1];
+			const geolocation = await fetchGeolocation(query);
+			if (!geolocation) throw new Error();
+			location.value = geolocation;
+		}, handleGeolocationError); fetchGeolocation
 	} else {
-		console.log('Geolocation is not supported by this browser.');
+		console.log("Geolocation is not supported by this browser.");
 	}
-	fetching.value = false
+	fetching.value = false;
 	return null;
 }
 
 function handleGeolocationError(error: GeolocationPositionError) {
 	switch (error.code) {
 		case error.PERMISSION_DENIED:
-			console.log('User denied the request for geolocation.');
+			console.log("User denied the request for geolocation.");
 			break;
 		case error.POSITION_UNAVAILABLE:
-			console.log('Geolocation information is unavailable.');
+			console.log("Geolocation information is unavailable.");
 			break;
 		case error.TIMEOUT:
-			console.log('The request to get user geolocation timed out.');
+			console.log("The request to get user geolocation timed out.");
 			break;
 		default:
-			console.log('An unknown geolocation error occurred.');
+			console.log("An unknown geolocation error occurred.");
 			break;
 	}
 }
-
-async function searchWeather(query: string) {
-	try {
-		if (!query) return;
-		const resGeocoding: AxiosResponse<GeocodingData[]> = await axios.get(
-			`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1appid=${import.meta.env.VITE_API_KEY}`
-		);
-		const resWeather: AxiosResponse<WeatherData> = await axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?lat=${resGeocoding.data[0].lat}&lon=${resGeocoding.data[0].lon}&&appid=${import.meta.env.VITE_API_KEY}`
-		);
-		apiData.value = resWeather.data;
-	} catch (error) {
-		console.error(error);
-	}
-}
-
 </script>
 <template>
-	<main>
-		<Controls @changeUnits="changeUnits" @searchWeather="searchWeather" :metric="metric" />
-		<Current :apiData="apiData" :metric="metric" :fetching="fetching" />
-		<Extra :apiData="apiData" :metric="metric" :fetching="fetching" />
-		<Forecast :apiData="apiData" :metric="metric" :fetching="fetching" />
+	<main v-if="apiData">
+		<div class="top">
+			<div class="left">
+				<Controls @changeUnits="changeUnits" @changeLocation="changeLocation" :metric="metric" />
+				<Current :data="apiData" :location="location" :metric="metric" :fetching="fetching" />
+			</div>
+			<Extra :data="apiData" :metric="metric" :fetching="fetching" />
+		</div>
+		<Forecast :data="apiData" :metric="metric" :fetching="fetching" />
 	</main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 main {
-	display: grid;
-	grid-template-columns: auto auto auto;
-	gap: 2rem;
+	width: 100%;
+	text-transform: capitalize;
+
+	.top {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.left {}
 }
 </style>
