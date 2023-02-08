a787a62bcd7124075687c292804053a2fb41d867
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-08 23:28:35 +0100

    Updating types and emit name.
    Other minor updates.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,28 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-const props = defineProps({
-	metric: Boolean,
-});
-const emit = defineEmits(['changeUnits', 'searchWeather']);
-const searchValue = ref('');
+import { ref } from "vue";
+
+const props = defineProps<{ metric: boolean }>();
+
+const emit = defineEmits(["changeUnits", "changeLocation"]);
+const searchValue = ref("");
 </script>
 <template>
 	<div class="controls">
-		<input
-			type="text"
-			id="search"
-			name="search"
-			v-model="searchValue"
-			placeholder="Search location..."
-			@keypress.enter="emit('searchWeather', searchValue)"
-		/>
+		<input type="text" id="search" name="search" v-model="searchValue" placeholder="Search location..."
+			@keypress.enter="emit('changeLocation', searchValue)" />
 		<button type="button" @click="emit('changeUnits')">
-			Use {{ metric ? 'imperial' : 'metric' }}
+			Use {{ metric? "imperial": "metric" }}
 		</button>
 	</div>
 </template>
-<style scoped>
+<style scoped lang="scss">
 .controls {
-	grid-column: 1/3;
+
+	button {
+		min-width: 100px;
+	}
 }
 </style>
