f75d36ede03c45873ad17538f8a9bd4f73e9c45d
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-09 21:45:41 +0100

    Adding function splitting long arrays into chunks.

---
+++ src/utilities/groupers.ts
@@ -0,0 +1,8 @@
+export function splitIntoChunks(data: any[], chunkSize: number): any[][] {
+	const array = [] as any[][];
+	for (let i = 0; i < data.length; i += chunkSize) {
+		const chunk = data.slice(i, i + chunkSize);
+		array.push(chunk);
+	}
+	return array;
+}
