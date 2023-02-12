536f287ea4ce9384f7750a82e9ca1d4e3415609f
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-12 22:27:01 +0100

    Renaming state vars.
    Adding watcher for pageView boolean to add event listener to weather
    forecast element allowing horizontal screel with mousewheel
    without the need to hold shift button.
    Fixing daily forecast not showing after switching to it from hourly with
    page set to higher number than daily forecast pages.
    Adding media query styling, layout styling,
    transition styling and pseudo classes styling.

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import type { OneCallWeatherData, HourlyForecast, DailyForecast } from "../types/types";
-import { convertTemp, convertSpeed } from "../utilities/converters";
-import { splitIntoGroups } from '../utilities/groupers';
+import { ref, watch } from "vue";
+import type { OneCallWeatherData } from "../types/types";
+import { convertTemp } from "../utilities/converters";
+import { splitIntoGroups } from "../utilities/groupers";
 
 const props = defineProps<{
 	data: OneCallWeatherData | null;
@@ -11,20 +11,20 @@ const props = defineProps<{
 }>();
 
 const displayedForecast = ref<"daily" | "hourly">("daily");
-const displayPages = ref<boolean>(true);
+const pageView = ref<boolean>(true);
 const currentPage = ref<number>(1);
-const groupSizes = ref<number>(8)
+const groupSizes = ref<number>(8);
 
 function changeForecast(value: "daily" | "hourly") {
 	displayedForecast.value = value;
 }
 
 function changeDisplay() {
-	displayPages.value = !displayPages.value;
+	pageView.value = !pageView.value;
 }
 
 function changePage(number: number) {
-	if (!props.data) return
+	if (!props.data) return;
 	if (number > Math.ceil(props.data[displayedForecast.value].length / 8)) {
 		currentPage.value = 1;
 	} else if (number < 1) {
@@ -35,6 +35,16 @@ function changePage(number: number) {
 		currentPage.value = number;
 	}
 }
+
+watch(pageView, () => {
+	// Find a way to cleanup event listener. !!!
+	const element = document.getElementById('forecast-display')
+	if (!element) return
+	element.addEventListener('wheel', (e) => {
+		element.scrollLeft += e.deltaY;
+	})
+})
+
 </script>
 <template>
 	<div v-if="!fetching && data" class="weather-forecast">
@@ -44,27 +54,29 @@ function changePage(number: number) {
 				<button type="button" @click="changeForecast('hourly')">Hourly</button>
 			</div>
 			<div class="display-controls" v-if="splitIntoGroups(data[displayedForecast], groupSizes).length > 1">
-				<div v-if="displayPages" class="page-controls">
+				<div v-if="pageView" class="page-controls">
 					<div class="arrow-prev" @click="changePage(currentPage - 1)">
-						prev
+						&#10094;
 					</div>
 					<div v-for="num in Math.ceil(data[displayedForecast].length / 8)" :key="num">
-						<span class="dot" :class="{ 'active': currentPage === num }" @click="currentPage = num"></span>
+						<span class="dot" :class="{ active: currentPage === num }" @click="currentPage = num"></span>
 					</div>
 					<div class="arrow-next" @click="changePage(currentPage + 1)">
-						next
+						&#10095;
 					</div>
 				</div>
 				<button type="button" @click="changeDisplay">
-					{{ displayPages? "Pages": "Scroll" }}
+					{{ pageView? "Pages": "Scroll" }}
 				</button>
 			</div>
 		</div>
-		<div class="forecast-display" :class="{ 'scroll': !displayPages }">
-			<div v-if="displayedForecast === 'daily'"
-				v-for="(chunk, index1) in (splitIntoGroups(data[displayedForecast], groupSizes) as DailyForecast[][])"
-				:key="index1" class="forecast-daily" :class="{ 'active': currentPage - 1 === index1 }">
-				<div v-for="(d, i) in chunk" :key="i">
+		<div id="forecast-display" :class="{ scroll: !pageView }">
+			<div v-if="displayedForecast === 'daily'" v-for="(chunk, index1) in splitIntoGroups(
+				data[displayedForecast],
+				groupSizes
+			)" :key="index1" class="forecast-daily"
+				:class="{ active: data[displayedForecast].length > groupSizes ? currentPage - 1 === index1 : 'active' }">
+				<div v-for="(d, i) in chunk" :key="i" class="day">
 					<div>
 						{{
 							new Date(d.dt * 1000).toLocaleString(undefined, {
@@ -81,15 +93,18 @@ function changePage(number: number) {
 							})
 						}}
 					</div>
-					<img :src="`https://openweathermap.org/img/wn/${d.weather[0].icon}.png`" :alt="d.weather[0].main" />
+					<div>
+						<img :src="`https://openweathermap.org/img/wn/${d.weather[0].icon}.png`" :alt="d.weather[0].main" />
+					</div>
 					<div>{{ d.weather[0].main }}</div>
 					<div>{{ convertTemp(metric, d.temp.day) }}</div>
 				</div>
 			</div>
-			<div v-else-if="displayedForecast === 'hourly'"
-				v-for="(chunk, index2) in (splitIntoGroups(data[displayedForecast], groupSizes) as HourlyForecast[][])"
-				:key="index2" class="forecast-hourly" :class="{ 'active': currentPage - 1 === index2 }">
-				<div v-for="(d, i) in chunk" :key="i">
+			<div v-else-if="displayedForecast === 'hourly'" v-for="(chunk, index2) in splitIntoGroups(
+				data[displayedForecast],
+				groupSizes
+			)" :key="index2" class="forecast-hourly" :class="{ active: currentPage - 1 === index2 }">
+				<div v-for="(d, i) in chunk" :key="i" class="hour">
 					<div>
 						{{
 							new Date(d.dt * 1000).toLocaleString(undefined, {
@@ -106,7 +121,9 @@ function changePage(number: number) {
 							})
 						}}
 					</div>
-					<img :src="`https://openweathermap.org/img/wn/${d.weather[0].icon}.png`" :alt="d.weather[0].main" />
+					<div>
+						<img :src="`https://openweathermap.org/img/wn/${d.weather[0].icon}.png`" :alt="d.weather[0].main" />
+					</div>
 					<div>{{ d.weather[0].main }}</div>
 					<div>{{ convertTemp(metric, d.temp) }}</div>
 				</div>
@@ -115,65 +132,78 @@ function changePage(number: number) {
 	</div>
 </template>
 <style scoped lang="scss">
-@import '../assets/base.scss';
+@import "../assets/base.scss";
+
 
 .weather-forecast {
 	display: flex;
 	flex-direction: column;
-	gap: 2rem;
+	gap: 1rem;
 }
 
 .forecast-controls {
 	display: flex;
 	justify-content: space-between;
+	gap: 1rem;
 
 	.display-controls {
 		display: flex;
 		align-items: center;
-		gap: 2rem;
+		gap: 1rem;
 	}
 
 	.time-controls {
 		display: flex;
 		align-items: center;
-		gap: 2rem;
+		gap: 1rem;
 	}
 
 	.page-controls {
 		display: flex;
 		align-items: center;
-		gap: 0.5rem;
+		gap: 0.25rem;
 
 		.dot {
 			display: block;
 			padding: 0.25rem;
 			margin: 0.25rem;
+			margin-top: 0.5rem;
 			border-radius: 50%;
-			border: 2px solid var(--vt-c-text-dark-2);
+			border: 2px solid var(--color-text);
 			user-select: none;
 			cursor: pointer;
+			transition: inherit;
+			transition: 0.25s ease-in-out;
+
+			&:hover {
+				background-color: var(--color-heading);
+			}
 
 			&.active {
-				background-color: var(--vt-c-text-dark-1);
+				background-color: var(--color-text);
 			}
 		}
 
 		.arrow-prev,
 		.arrow-next {
-			padding: 2px;
 			user-select: none;
 			cursor: pointer;
+			margin: 0 0.25rem;
+			transition: 0.25s ease-in-out;
+
+			&:hover {
+				filter: brightness(75%);
+			}
 		}
 	}
 }
 
-.forecast-display {
+#forecast-display {
 	display: flex;
-	padding-bottom: 1rem;
-	gap: 2rem;
+	flex-direction: column;
+	text-align: center;
 
 	&.scroll {
-		overflow-x: scroll;
 
 		.forecast-daily,
 		.forecast-hourly {
@@ -185,13 +215,60 @@ function changePage(number: number) {
 .forecast-daily,
 .forecast-hourly {
 	display: none;
-	gap: 2rem;
+	flex-direction: column;
 
 	&.active {
 		flex: 1;
 		justify-content: space-between;
 		flex-wrap: nowrap;
 		display: flex;
+	}
+
+	.day,
+	.hour {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 1rem;
+		padding: 0 1rem;
+		border-radius: 8px;
+
+		&:nth-child(2n) {
+			background-color: var(--color-background);
+		}
+
+		>* {
+			flex: 1;
+		}
+	}
+}
+
+@media (min-width: 992px) {
+
+	#forecast-display {
+		flex-direction: row;
+
+		&.scroll {
+			overflow-x: scroll;
+			scrollbar-width: 0;
+			padding-bottom: 1rem;
+
+			.forecast-daily,
+			.forecast-hourly {
+				gap: 0.5rem;
+			}
+		}
+	}
+
+	.forecast-daily,
+	.forecast-hourly {
+		flex-direction: row;
+
+		.day,
+		.hour {
+			flex-direction: column;
+			padding: 1rem;
+		}
 	}
 }
 </style>
