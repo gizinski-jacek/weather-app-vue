6a0420e654440ab85e042d1072115bbe52af870b
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-14 23:59:03 +0100

    Importing and implementing degrees converter.
    Updating displayed data.
    Changing flex layout to grid.

--- src/components/Extra.vue
+++ src/components/Extra.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import type { OneCallWeatherData } from "../types/types";
-import { convertSpeed, convertVisibility } from "../utilities/converters";
+import {
+	convertSpeed,
+	convertVisibility,
+	degreesToCompassDirection
+} from "../utilities/converters";
 
 const props = defineProps<{
 	data: OneCallWeatherData | null;
@@ -12,20 +16,25 @@ const props = defineProps<{
 	<div v-if="!fetching && data" class="extra-widgets">
 		<p>Pressure: {{ data.current.pressure }} hPa</p>
 		<p>Visibility: {{ convertVisibility(metric, data.current.visibility) }}</p>
-		<p>Wind speed: {{ convertSpeed(metric, data.current.wind_speed) }}</p>
-		<p>Wind deg: {{ data.current.wind_deg }}</p>
+		<p>Wind speed: {{ convertSpeed(metric, data.current.wind_speed) }} {{
+			degreesToCompassDirection(data.current.wind_deg)
+		}}</p>
 		<p v-if="data.current.wind_gust">
 			Wind gust: {{ convertSpeed(metric, data.current.wind_gust) }}
 		</p>
-		<p>Cloudiness: {{ data.current.clouds }}</p>
+		<p>Humidity: {{ data.current.humidity }}%</p>
+		<p>UV: {{ data.current.uvi }}</p>
 		<p v-if="data.current.rain">Rain 1h: {{ data.current.rain["1h"] }}</p>
 		<p v-if="data.current.snow">Snow 1h: {{ data.current.snow["1h"] }}</p>
+		<p>Dew point: {{ data.current.dew_point }}°C</p>
 	</div>
 </template>
 <style scoped lang="scss">
 .extra-widgets {
-	display: flex;
-	flex-direction: row;
+	display: grid;
+	grid-template-rows: auto auto;
+	grid-auto-flow: column;
+	grid-gap: 1rem;
 
 	>p {
 		margin: 0.5rem;
@@ -34,7 +43,8 @@ const props = defineProps<{
 
 @media (min-width: 768px) {
 	.extra-widgets {
-		flex-direction: column;
+		grid-template-columns: auto auto;
+		grid-auto-flow: row;
 
 		span {
 			flex-direction: row;
