a25ecc2b8eb24f76de36d9a62e026551be657544
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-15 00:01:30 +0100

    Importing and implementing wind description converter.
    Cleaning up styling.

--- src/components/Current.vue
+++ src/components/Current.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { convertTemp } from "@/utilities/converters";
+import { convertTemp, windSpeedToDescription } from "@/utilities/converters";
 import type { GeocodingData, OneCallWeatherData } from "../types/types";
 
 const props = defineProps<{
@@ -12,44 +12,47 @@ const props = defineProps<{
 <template>
 	<div v-if="!fetching && data && location" class="current-weather">
 		<div class="left">
-			<h2>{{ location.name }}, {{ location.country }}</h2>
-			<p>{{ location.state }}</p>
-			<p>
+			<p class="date">
 				{{
 					new Date(data.current.dt * 1000).toLocaleString(undefined, {
 						weekday: "long",
 						month: "numeric",
 						day: "numeric",
-						year: "numeric",
 						hour: "numeric",
 						minute: "numeric",
 						second: "numeric",
 					})
 				}}
 			</p>
+			<h2>{{ location.name }}, {{ location.country }}</h2>
+			<p>{{ location.state }}</p>
 		</div>
-		<div class="right">
+		<div class="center">
 			<span>
 				<img :src="`https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`"
 					:alt="data.current.weather[0].description" />
 				<h1>{{ convertTemp(metric, data.current.temp) }}</h1>
 			</span>
+		</div>
+		<div class="right">
 			<p>Feels like: {{ convertTemp(metric, data.current.feels_like) }}</p>
 			<p>{{ data.current.weather[0].description }}</p>
+			<p>{{ windSpeedToDescription(data.current.wind_speed) }}</p>
 		</div>
 	</div>
 </template>
 <style scoped lang="scss">
 .current-weather {
 	display: flex;
-	justify-content: space-evenly;
+	justify-content: space-between;
 	gap: 1rem;
-	margin: 0.5rem 0;
+
+	.date {
+		color: var(--color-text-alt)
+	}
 
 	span {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
+		text-align: center;
 	}
 
 	>div {
@@ -61,11 +64,7 @@ const props = defineProps<{
 
 @media (min-width: 768px) {
 	.current-weather {
-		justify-content: flex-start;
-
-		span {
-			flex-direction: row;
-		}
+		gap: 2rem;
 	}
 }
 </style>
