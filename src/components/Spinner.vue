60faf5300508c3b530c6ec17373cfcd310377eb8
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-18 20:01:00 +0100

    Adding styled spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,30 @@
+
+
+<script setup lang="ts">
+</script>
+<template>
+	<div class="spinner"></div>
+</template>
+
+
+<style scoped lang="scss">
+.spinner {
+	margin: 10rem auto;
+	border: 16px solid var(--color-background);
+	border-top: 16px solid var(--color-text-alt);
+	border-radius: 50%;
+	width: 160px;
+	height: 160px;
+	animation: spin 1.5s linear infinite;
+}
+
+@keyframes spin {
+	0% {
+		transform: rotate(0deg);
+	}
+
+	100% {
+		transform: rotate(360deg);
+	}
+}
+</style>
