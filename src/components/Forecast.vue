91c52fe42e54b14547efa0f2d403cea2fae464e6
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-18 20:12:55 +0100

    Removing event listener from forecast-display
    element when changing back to page view.
    Replacing static number with groupSize state value for
    various calculations related to forecast paging and rendering.
    Adjusting styling on higher viewports with grid layout.
    Minor button, dots and arrows styling.
    Cleaning up unneeded classes and styling.

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -13,7 +13,7 @@ const props = defineProps<{
 const displayedForecast = ref<"daily" | "hourly">("daily");
 const pageView = ref<boolean>(true);
 const currentPage = ref<number>(1);
-const groupSizes = ref<number>(8);
+const groupSize = ref<number>(8);
 const smallScreen = ref<boolean>(false);
 
 onMounted(() => {
@@ -43,24 +43,34 @@ function changeDisplay() {
 
 function changePage(number: number) {
 	if (!props.data) return;
-	if (number > Math.ceil(props.data[displayedForecast.value].length / 8)) {
+	if (
+		number >
+		Math.ceil(props.data[displayedForecast.value].length / groupSize.value)
+	) {
 		currentPage.value = 1;
 	} else if (number < 1) {
 		currentPage.value = Math.ceil(
-			props.data[displayedForecast.value].length / 8
+			props.data[displayedForecast.value].length / groupSize.value
 		);
 	} else {
 		currentPage.value = number;
 	}
 }
 
-watch(pageView, () => {
-	// Find a way to cleanup event listener. !!!
+function scrollHorizontally(e: WheelEvent) {
 	const element = document.getElementById("forecast-display");
 	if (!element) return;
-	element.addEventListener("wheel", (e: WheelEvent) => {
-		element.scrollLeft += e.deltaY;
-	});
+	element.scrollLeft += e.deltaY;
+}
+
+watch(pageView, () => {
+	const element = document.getElementById("forecast-display");
+	if (!element) return;
+	if (pageView.value) {
+		element.removeEventListener("wheel", scrollHorizontally);
+	} else {
+		element.addEventListener("wheel", scrollHorizontally)
+	}
 });
 </script>
 <template>
@@ -74,31 +84,31 @@ watch(pageView, () => {
 					Hourly
 				</button>
 			</div>
-			<div class="display-controls" v-if="splitIntoGroups(data[displayedForecast], groupSizes).length > 1">
+			<div class="display-controls" v-if="splitIntoGroups(data[displayedForecast], groupSize).length > 1">
 				<div v-if="pageView" class="page-controls">
 					<div class="arrow-prev" @click="changePage(currentPage - 1)"></div>
-					<div v-for="num in Math.ceil(data[displayedForecast].length / 8)" :key="num">
+					<div v-for="num in Math.ceil(data[displayedForecast].length / groupSize)" :key="num">
 						<span class="dot" :class="{ active: currentPage === num }" @click="currentPage = num"></span>
 					</div>
 					<div class="arrow-next" @click="changePage(currentPage + 1)"></div>
 				</div>
 				<button type="button" @click="changeDisplay">
-					{{ pageView? "Pages": "Scroll" }}
+					{{ pageView ? "Pages" : "Scroll" }}
 				</button>
 			</div>
 		</div>
 		<div id="forecast-display" :class="{ scroll: !pageView }">
 			<div v-if="displayedForecast === 'daily'" v-for="(chunk, indexH) in splitIntoGroups(
 				data[displayedForecast],
-				groupSizes
+				groupSize
 			)" :key="indexH" class="forecast-daily" :class="{
 	active:
-		data[displayedForecast].length > groupSizes
+		data[displayedForecast].length > groupSize
 			? currentPage - 1 === indexH
 			: 'active',
 }">
 				<div v-for="(d, i) in chunk" :key="i" class="day">
-					<div class="date">
+					<div>
 						{{
 							new Date(d.dt * 1000).toLocaleString(undefined, {
 								weekday: smallScreen ? "short" : "long",
@@ -111,15 +121,15 @@ watch(pageView, () => {
 						<img :src="`https://openweathermap.org/img/wn/${d.weather[0].icon}.png`" :alt="d.weather[0].main" />
 					</div>
 					<div>{{ convertTemp(metric, d.temp.day) }}</div>
-					<div class="description">{{ d.weather[0].description }}</div>
+					<div>{{ d.weather[0].description }}</div>
 				</div>
 			</div>
 			<div v-else-if="displayedForecast === 'hourly'" v-for="(chunk, indexD) in splitIntoGroups(
 				data[displayedForecast],
-				groupSizes
+				groupSize
 			)" :key="indexD" class="forecast-hourly" :class="{ active: currentPage - 1 === indexD }">
 				<div v-for="(h, i) in chunk" :key="i" class="hour">
-					<div class="date">
+					<div>
 						{{
 							new Date(h.dt * 1000).toLocaleString(undefined, {
 								weekday: smallScreen ? "short" : "long",
@@ -134,7 +144,7 @@ watch(pageView, () => {
 						<img :src="`https://openweathermap.org/img/wn/${h.weather[0].icon}.png`" :alt="h.weather[0].main" />
 					</div>
 					<div>{{ convertTemp(metric, h.temp) }}</div>
-					<div class="description">{{ h.weather[0].description }}</div>
+					<div>{{ h.weather[0].description }}</div>
 				</div>
 			</div>
 		</div>
@@ -147,6 +157,11 @@ watch(pageView, () => {
 	display: flex;
 	flex-direction: column;
 	gap: 1rem;
+
+	button.active {
+		color: var(--color-text-alt);
+		border-color: var(--color-text);
+	}
 }
 
 .forecast-controls {
@@ -158,14 +173,13 @@ watch(pageView, () => {
 
 	.time-controls {
 		button:not(.active) {
-			color: var(--color-heading);
 			background-color: transparent;
-			border: 2px solid;
 			border-color: var(--color-heading) transparent;
 			border-radius: 0;
 
 			&:hover {
-				border-color: var(--color-heading);
+				background-color: var(--color-background);
+				border-color: var(--color-text);
 				border-radius: 4px;
 			}
 		}
@@ -203,9 +217,15 @@ watch(pageView, () => {
 				background-color: var(--color-border-hover);
 			}
 
+			&:active {
+				background-color: var(--color-text-alt);
+			}
+
 			&.active {
 				background-color: var(--color-text);
 			}
+
+
 		}
 
 		.arrow-prev,
@@ -232,6 +252,11 @@ watch(pageView, () => {
 			&:hover:after {
 				background-color: var(--color-text);
 			}
+
+			&:active:before,
+			&:active:after {
+				background-color: var(--color-text-alt);
+			}
 		}
 
 		.arrow-next {
@@ -250,6 +275,7 @@ watch(pageView, () => {
 		.forecast-daily,
 		.forecast-hourly {
 			display: flex;
+			flex-direction: column;
 		}
 	}
 }
@@ -257,20 +283,18 @@ watch(pageView, () => {
 .forecast-daily,
 .forecast-hourly {
 	display: none;
-	flex-direction: column;
 	min-height: 280px;
 
 	&.active {
-		justify-content: space-between;
-		flex-wrap: nowrap;
 		display: flex;
+		flex-direction: column;
 	}
 
 	.day,
 	.hour {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
+		display: grid;
+		grid-template-columns: 1fr 50px 50px 1fr;
+		grid-auto-flow: column;
 		align-items: center;
 		padding: 0 1rem;
 		gap: 1rem;
@@ -279,24 +303,20 @@ watch(pageView, () => {
 		&:nth-child(2n) {
 			background-color: var(--color-background);
 		}
-
-		.date,
-		.description {
-			flex: 1;
-		}
 	}
 }
 
-@media (min-width: 540px) {
+@media (min-width: 640px) {
 	.forecast-controls {
 		flex-direction: row;
+		flex-wrap: nowrap;
 	}
 }
 
 @media (min-width: 940px) {
+
 	#forecast-display {
 		flex-direction: row;
-		max-width: 1200px;
 		padding-bottom: 1rem;
 
 		&.scroll {
@@ -304,20 +324,27 @@ watch(pageView, () => {
 
 			.forecast-daily,
 			.forecast-hourly {
-				gap: 0.5rem;
+				display: grid;
+				grid-template-columns: auto;
+				min-width: 100%;
 			}
 		}
 	}
 
 	.forecast-daily,
 	.forecast-hourly {
-		flex-direction: row;
+		grid-auto-flow: column;
+
+		&.active {
+			display: grid;
+			grid-template-columns: auto;
+		}
 
 		.day,
 		.hour {
+			display: flex;
 			flex-direction: column;
 			padding: 1rem;
-			flex: 1 1 0px;
 		}
 	}
 }
