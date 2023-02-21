8bc626683205643ed63b5d4a44f4e218dc274def
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-21 22:48:25 +0100

    Adding AirPollution interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -92,3 +92,21 @@ export interface DailyForecast {
 	wind_gust?: number;
 	wind_speed: number;
 }
+
+export interface AirPollution {
+	coord: { lat: number; lon: number };
+	list: {
+		dt: number;
+		main: { aqi: number };
+		components: {
+			co: number;
+			no: number;
+			no2: number;
+			o3: number;
+			so2: number;
+			pm2_5: number;
+			pm10: number;
+			nh3: number;
+		};
+	}[];
+}
