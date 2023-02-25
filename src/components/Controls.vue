453ab5351592e00b16e7b8b5c2f13181473ea9bc
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-25 20:22:28 +0100

    Adding button to trigger geolocation request.
    Styling said button.
    Trimming input before emitting in search function.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -3,7 +3,7 @@ import { ref } from "vue";
 
 const props = defineProps<{ metric: boolean }>();
 
-const emit = defineEmits(["changeUnits", "changeLocation"]);
+const emit = defineEmits(["changeUnits", "searchLocation", "requestUserGeolocation"]);
 const searchValue = ref<string>("");
 const input = ref<HTMLInputElement | null>(null);
 const svgBtn = ref<SVGElement | null>(null);
@@ -13,7 +13,7 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 		if (e.target === svgBtn.value) input.value?.focus();
 		return;
 	} else {
-		emit("changeLocation", searchValue.value);
+		emit("searchLocation", searchValue.value.trim());
 		searchValue.value = "";
 	}
 }
@@ -29,6 +29,11 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 				<path d="M14.5 14.5L19 19" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
 			</svg>
 		</div>
+		<div class="geolocation" @click="emit('requestUserGeolocation')">
+			<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+				<path d="M25.497 6.503l.001-.003-.004.005L3.5 15.901l11.112 1.489 1.487 11.11 9.396-21.992.005-.006z" />
+			</svg>
+		</div>
 		<button type="button" @click="emit('changeUnits')">
 			Use {{ metric ? "imperial" : "metric" }}
 		</button>
@@ -40,7 +45,6 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 	display: flex;
 	justify-content: space-between;
 	gap: 1rem;
-	margin-bottom: 1rem;
 	text-transform: capitalize;
 
 	button {
@@ -72,8 +76,8 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 	}
 
 	svg {
-		width: 1.75rem;
-		height: 1.75rem;
+		width: 28px;
+		height: 28px;
 		fill: none;
 		margin: auto 0;
 		margin-right: 0;
@@ -92,6 +96,23 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 				stroke: var(--color-text-alt);
 			}
 		}
+	}
+
+	.search-results {
+		display: flex;
+	}
+}
+
+.geolocation {
+	width: 28px;
+	height: 28px;
+	margin: auto;
+
+	svg {
+		width: 28px;
+		height: 28px;
+		fill: var(--color-text-alt);
+		cursor: pointer;
 	}
 }
 </style>
