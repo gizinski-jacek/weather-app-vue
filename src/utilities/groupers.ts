0940a1ad5498c7088c02bf096d76102f11d56864
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-25 20:07:21 +0100

    Removing file, moving function to converters.

--- src/utilities/groupers.ts
+++
@@ -1,9 +0,0 @@
-// Splits long array into smaller groups.
-export function splitIntoGroups(data: any[], chunkSize: number): any[][] {
-	const array = [] as any[][];
-	for (let i = 0; i < data.length; i += chunkSize) {
-		const chunk = data.slice(i, i + chunkSize);
-		array.push(chunk);
-	}
-	return array;
-}
