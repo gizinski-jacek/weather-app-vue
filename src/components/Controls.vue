56a98b62971375f847cd2dfe0a8fcfc89ba6ab62
Jacek Gizinski <jacektrg+github@gmail.com>
2023-02-27 23:31:02 +0100

    Adding clear input button.
    Updating units system button.
    Updating mag-icon styling.
    Styling geolocation button and clear input button.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { GeocodingData } from '@/types/types';
+import { roundToDecimal } from '@/utilities/converters';
 import { ref } from "vue";
 
 const props = defineProps<{ metric: boolean, searchResults: GeocodingData[] | null }>();
@@ -8,6 +9,10 @@ const emit = defineEmits(["changeUnits", "searchLocation", "requestUserGeolocati
 const searchValue = ref<string>("");
 const input = ref<HTMLInputElement | null>(null);
 const svgBtn = ref<SVGElement | null>(null);
+
+function clearInput() {
+	searchValue.value = "";
+}
 
 function triggerSearch(e: KeyboardEvent | MouseEvent) {
 	if (!searchValue.value) {
@@ -25,26 +30,30 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 		<div class="search">
 			<input ref="input" type="text" id="search" name="search" v-model="searchValue" placeholder="Search city..."
 				@keypress.enter="triggerSearch" />
-			<svg ref="svgBtn" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" @click="triggerSearch">
-				<circle cx="10" cy="10" r="6" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-				<path d="M14.5 14.5L19 19" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-			</svg>
+			<div v-if="searchValue" class="clear" @click="clearInput"></div>
+			<div class="mag-icon">
+				<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" @click="triggerSearch">
+					<circle cx="10" cy="10" r="6" />
+					<path d="M14.5 14.5L19 19" />
+				</svg>
+			</div>
 			<div v-if="searchResults" class="search-results">
 				<div v-for="(result, index) in searchResults" :key="index" class="result" @click="emit('searchLocation',
 					result)">
 					<div>{{ result.country }}</div>
 					<div>{{ result.name }}</div>
-					<div>{{ result.lat.toFixed(3) }}, {{ result.lon.toFixed(3) }}</div>
+					<!-- Not working with roundToDecimal() -->
+					<div>{{ roundToDecimal(result.lat, 3) }}, {{ roundToDecimal(result.lon, 3) }}</div>
 				</div>
 			</div>
 		</div>
-		<div class="geolocation" @click="emit('requestUserGeolocation')">
+		<button type="button" class="geolocation" @click="emit('requestUserGeolocation')">
 			<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
 				<path d="M25.497 6.503l.001-.003-.004.005L3.5 15.901l11.112 1.489 1.487 11.11 9.396-21.992.005-.006z" />
 			</svg>
-		</div>
-		<button type="button" @click="emit('changeUnits')">
-			Use {{ metric ? "imperial" : "metric" }}
+		</button>
+		<button type="button" class="units" @click="emit('changeUnits')">
+			{{ metric ? "imperial" : "metric" }}
 		</button>
 	</div>
 </template>
@@ -53,22 +62,23 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 .controls {
 	display: flex;
 	justify-content: space-between;
-	gap: 1rem;
+	gap: 0.5rem;
 	text-transform: capitalize;
 
-	button {
-		min-width: 110px;
+	.units {
+		min-width: 84px;
 	}
+
 }
 
 .search {
 	flex: 1;
-	max-width: 300px;
 	display: flex;
 	width: fit-content;
 	background-color: var(--color-background);
 	border: 2px solid var(--color-text);
 	border-radius: 4px;
+	gap: 0.25rem;
 	position: relative;
 
 	input {
@@ -85,28 +95,44 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 		}
 	}
 
-	svg {
-		width: 28px;
-		height: 28px;
-		fill: none;
+	.mag-icon {
 		margin: auto 0;
-		margin-right: 0;
-		margin-left: auto;
-		cursor: pointer;
 
-		circle,
-		path {
-			stroke: var(--color-text);
-		}
-
-		&:active {
+		svg {
+			display: block;
+			width: 24px;
+			height: 24px;
+			fill: none;
+			cursor: pointer;
+			stroke-width: 2px;
+			stroke-linecap: round;
+			transition: 0.25s ease-in-out;
 
 			circle,
 			path {
-				stroke: var(--color-text-alt);
+				stroke: var(--color-heading);
+				transition: inherit;
+			}
+
+			&:hover {
+
+				circle,
+				path {
+					stroke: var(--color-text);
+				}
+			}
+
+			&:active {
+
+				circle,
+				path {
+					stroke: var(--color-text-alt);
+				}
 			}
 		}
 	}
+
+
 
 	.search-results {
 		position: absolute;
@@ -123,6 +149,10 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 			border: 2px solid var(--color-text);
 			border-bottom-width: 0;
 			cursor: pointer;
+
+			>div:first-child {
+				width: 28px;
+			}
 
 			>div:nth-child(2) {
 				flex: 1;
@@ -149,15 +179,51 @@ function triggerSearch(e: KeyboardEvent | MouseEvent) {
 }
 
 .geolocation {
-	width: 28px;
-	height: 28px;
-	margin: auto;
+	padding: 0 0.1rem;
+	margin-left: auto;
+
+	&:hover {
+		border: 2px solid var(--color-text);
+	}
+
+	&:active {
+		border: 2px solid var(--color-text-alt);
+	}
 
 	svg {
+		display: block;
 		width: 28px;
 		height: 28px;
 		fill: var(--color-text-alt);
-		cursor: pointer;
+	}
+}
+
+.clear {
+	cursor: pointer;
+
+	&:before,
+	&:after {
+		display: block;
+		content: "";
+		width: 20px;
+		height: 2px;
+		background-color: var(--color-heading);
+		transform: translateY(15px) rotate(-45deg);
+		transition: 0.25s ease-in-out;
+	}
+
+	&:after {
+		transform: translateY(13px) rotate(45deg);
+	}
+
+	&:hover:before,
+	&:hover:after {
+		background-color: var(--color-text);
+	}
+
+	&:active:before,
+	&:active:after {
+		background-color: var(--color-text-alt);
 	}
 }
 </style>
