64787b05f39fd08da6d455b4e03f05793086821b
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-14 21:53:33 +0100

    Adding sunset and sunrise information.
    Adjusting styling.

--- src/components/Current.vue
+++ src/components/Current.vue
@@ -46,6 +46,12 @@ const props = defineProps<{
 			<span>{{ weather.current.weather[0].description }}</span>
 			<span>{{ windSpeedToDescription(weather.current.wind_speed) }}</span>
 		</div>
+		<div>
+			<span>Sunrise: {{ new Date(weather.current.sunrise * 1000).toLocaleString(undefined,
+				{ hour: 'numeric', minute: 'numeric' }) }}</span>
+			<span>Sunset: {{ new Date(weather.current.sunset * 1000).toLocaleString(undefined,
+				{ hour: 'numeric', minute: 'numeric' }) }}</span>
+		</div>
 	</div>
 </template>
 
@@ -63,19 +69,13 @@ const props = defineProps<{
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		gap: 2rem;
+		gap: 3rem;
 		text-align: center;
 	}
 
 	.date {
 		color: var(--color-text-alt);
 		font-weight: 600;
-	}
-}
-
-@media (min-width:640px) {
-	.current-weather {
-		align-items: unset;
 	}
 }
 </style>
