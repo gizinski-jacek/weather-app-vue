a7f737626a09ca48094eb9c2b3068d2855876268
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-20 19:07:19 +0100

    Adding and styling day/night temps display.
    Adjusting grid columns sizes.
    Adding tooltip-icon class for styling and adjusting styling.

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -146,9 +146,55 @@ watch(pageView, () => {
 					<div>
 						<img :src="`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`" :alt="day.weather[0].main" />
 					</div>
-					<div>{{ convertTemp(metric, day.temp.day) }}</div>
+					<div class="temperature">
+						<div>
+							<svg class="sun" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<rect width="24" height="24" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12Z"
+									fill="#323232" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M12 2C12.5523 2 13 2.44772 13 3V5C13 5.55228 12.5523 6 12 6C11.4477 6 11 5.55228 11 5V3C11 2.44772 11.4477 2 12 2Z"
+									fill="#323232" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M19.7071 4.29289C20.0976 4.68342 20.0976 5.31658 19.7071 5.70711L17.7071 7.70711C17.3166 8.09763 16.6834 8.09763 16.2929 7.70711C15.9024 7.31658 15.9024 6.68342 16.2929 6.29289L18.2929 4.29289C18.6834 3.90237 19.3166 3.90237 19.7071 4.29289Z"
+									fill="#323232" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M18 12C18 11.4477 18.4477 11 19 11H21C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13H19C18.4477 13 18 12.5523 18 12Z"
+									fill="#323232" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M16.2929 16.2929C16.6834 15.9024 17.3166 15.9024 17.7071 16.2929L19.7071 18.2929C20.0976 18.6834 20.0976 19.3166 19.7071 19.7071C19.3166 20.0976 18.6834 20.0976 18.2929 19.7071L16.2929 17.7071C15.9024 17.3166 15.9024 16.6834 16.2929 16.2929Z"
+									fill="#323232" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M12 18C12.5523 18 13 18.4477 13 19V21C13 21.5523 12.5523 22 12 22C11.4477 22 11 21.5523 11 21V19C11 18.4477 11.4477 18 12 18Z"
+									fill="#323232" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M7.70711 16.2929C8.09763 16.6834 8.09763 17.3166 7.70711 17.7071L5.70711 19.7071C5.31658 20.0976 4.68342 20.0976 4.29289 19.7071C3.90237 19.3166 3.90237 18.6834 4.29289 18.2929L6.29289 16.2929C6.68342 15.9024 7.31658 15.9024 7.70711 16.2929Z"
+									fill="#323232" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M2 12C2 11.4477 2.44772 11 3 11H5C5.55228 11 6 11.4477 6 12C6 12.5523 5.55228 13 5 13H3C2.44772 13 2 12.5523 2 12Z"
+									fill="#323232" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M4.29289 4.29289C4.68342 3.90237 5.31658 3.90237 5.70711 4.29289L7.70711 6.29289C8.09763 6.68342 8.09763 7.31658 7.70711 7.70711C7.31658 8.09763 6.68342 8.09763 6.29289 7.70711L4.29289 5.70711C3.90237 5.31658 3.90237 4.68342 4.29289 4.29289Z"
+									fill="#323232" />
+							</svg>
+							<span>
+								{{ convertTemp(metric, day.temp.day) }}
+							</span>
+						</div>
+						<div>
+							<svg class="moon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<rect width="24" height="24" />
+								<path fill-rule="evenodd" clip-rule="evenodd"
+									d="M8.23129 2.24048C9.24338 1.78695 10.1202 2.81145 9.80357 3.70098C8.72924 6.71928 9.38932 10.1474 11.6193 12.3765C13.8606 14.617 17.3114 15.2755 20.3395 14.1819C21.2206 13.8637 22.2173 14.7319 21.7817 15.7199C21.7688 15.7491 21.7558 15.7782 21.7427 15.8074C20.9674 17.5266 19.7272 19.1434 18.1227 20.2274C16.4125 21.3828 14.3957 22.0001 12.3316 22.0001H12.3306C9.93035 21.9975 7.6057 21.1603 5.75517 19.6321C3.90463 18.1039 2.64345 15.9797 2.18793 13.6237C1.73241 11.2677 2.11094 8.82672 3.2586 6.71917C4.34658 4.72121 6.17608 3.16858 8.20153 2.25386L8.23129 2.24048Z" />
+							</svg>
+							<span>
+								{{ convertTemp(metric, day.temp.night) }}
+							</span>
+						</div>
+					</div>
 					<div>{{ day.weather[0].description }}</div>
-					<div>
+					<div class="tooltip-icon">
 						<Tooltip>
 							<div class="tooltip">
 								<div v-if="day.rain" class="precipitation">
@@ -305,7 +351,7 @@ watch(pageView, () => {
 					</div>
 					<div>{{ convertTemp(metric, hour.temp) }}</div>
 					<div>{{ hour.weather[0].description }}</div>
-					<div>
+					<div class="tooltip-icon">
 						<Tooltip>
 							<div class="tooltip">
 								<div v-if="hour.rain" class="precipitation">
@@ -583,7 +629,7 @@ watch(pageView, () => {
 .forecast-hourly {
 	display: none;
 	flex: 1;
-	min-height: 260px;
+	min-height: 280px;
 
 	&.active {
 		display: flex;
@@ -594,10 +640,10 @@ watch(pageView, () => {
 .day,
 .hour {
 	display: grid;
-	grid-template-columns: 1fr 50px 50px 1fr 24px;
+	grid-template-columns: 1fr 50px 80px 1fr 20px;
 	grid-auto-flow: column;
 	align-items: center;
-	padding: 0 1rem;
+	padding: 0 0.5rem;
 	gap: 0.5rem;
 	border-radius: 8px;
 
@@ -605,23 +651,42 @@ watch(pageView, () => {
 		background-color: var(--color-background);
 	}
 
-	>div:last-child {
-		width: 24px;
-		height: 24px;
+	.tooltip-icon {
+		width: 20px;
+		height: 20px;
 
-		svg {
-			display: block;
-			width: 24px;
-			height: 24px;
+		>svg {
 			fill: transparent;
+			stroke: var(--color-text);
+		}
+	}
 
-			path {
-				stroke: var(--color-text);
+	.temperature {
+		>div {
+			display: flex;
+			justify-content: space-evenly;
+			align-items: center;
+			flex-wrap: wrap;
+			gap: 0.25rem;
+
+			svg {
+				width: 20px;
+				height: 20px;
+
+				path {
+					fill: var(--color-text-alt);
+				}
+			}
+
+			.moon path {
+				fill: var(--vt-c-border-dark);
 			}
 		}
-
-
 	}
+}
+
+.hour {
+	grid-template-columns: 1fr 50px 50px 1fr 20px;
 }
 
 .tooltip {
@@ -644,8 +709,9 @@ watch(pageView, () => {
 		}
 
 		svg {
+			width: 24px;
+			height: 24px;
 			fill: var(--color-text);
-			stroke-width: 0.5px;
 		}
 	}
 }
@@ -654,6 +720,10 @@ watch(pageView, () => {
 	.forecast-controls {
 		flex-direction: row;
 		flex-wrap: nowrap;
+	}
+
+	.day .temperature>div {
+		justify-content: space-between;
 	}
 }
 
@@ -698,12 +768,11 @@ watch(pageView, () => {
 	.hour {
 		display: grid;
 		grid-template-columns: unset;
-		grid-template-rows: 1fr 50px 50px 1fr 24px;
+		grid-template-rows: 1fr 50px 50px 1fr 20px;
 		grid-auto-flow: column;
 		justify-items: center;
 		align-items: start;
 		padding: 1rem;
-		gap: 0;
 	}
 }
 </style>
