caaf5e04165d88a4a351358f35b81f8a6308ad91
Jacek Gizinski <jacektrg+github@gmail.com>
2025-05-16 16:36:34 +0200

    Renaming functions.
    Adding function getting and returning data for both weather and air pollution.
    Moving in function from App component handling getting user's geolocation.

--- src/utilities/fetchers.ts
+++ src/utilities/fetchers.ts
@@ -5,8 +5,72 @@ import type {
 	OneCallWeatherData,
 	AirPollution,
 } from '@/types/types';
+import { Capacitor } from '@capacitor/core';
+import { Preferences } from '@capacitor/preferences';
+import { Geolocation } from '@capacitor/geolocation';
+import type { Position } from '@capacitor/geolocation/dist/esm';
 
-export async function fetchByQuery(query: string): Promise<GeocodingData[]> {
+export function getLocation(): Promise<GeocodingData> {
+	return new Promise(async (resolve, reject) => {
+		try {
+			if (Capacitor.isNativePlatform()) {
+				const geolocation: Position = await Geolocation.getCurrentPosition();
+				const locationData = await getLocationByCoords(
+					geolocation.coords.latitude,
+					geolocation.coords.longitude
+				);
+				await Preferences.set({
+					key: 'userLocation',
+					value: JSON.stringify(locationData),
+				});
+				resolve(locationData);
+			} else {
+				if (navigator.geolocation) {
+					navigator.geolocation.getCurrentPosition(
+						async (position) => {
+							const { latitude, longitude } = position.coords;
+							const geolocation = await getLocationByCoords(
+								latitude,
+								longitude
+							);
+							localStorage.setItem('userLocation', JSON.stringify(geolocation));
+							resolve(geolocation);
+						},
+						(error) => {
+							switch (error.code) {
+								case 1:
+									reject(
+										new Error(
+											'Geolocation request denied. Change browser setting to allow geolocation to use this function.'
+										)
+									);
+									break;
+								case 2:
+									reject(new Error('Geolocation unavailable.'));
+									break;
+								case 3:
+									reject(new Error('Geolocation request timeout.'));
+									break;
+								default:
+									reject(new Error('Geolocation request error.'));
+									break;
+							}
+						},
+						{ timeout: 5000 }
+					);
+				} else {
+					reject(new Error('Geolocation is not supported by this browser.'));
+				}
+			}
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+
+export async function getLocationByQuery(
+	query: string
+): Promise<GeocodingData[]> {
 	try {
 		const res: AxiosResponse<GeocodingData[]> = await axios.get(
 			`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${
@@ -20,7 +84,7 @@ export async function fetchByQuery(query: string): Promise<GeocodingData[]> {
 	}
 }
 
-export async function fetchByCoords(
+export async function getLocationByCoords(
 	lat: number,
 	lon: number
 ): Promise<GeocodingData> {
@@ -37,7 +101,7 @@ export async function fetchByCoords(
 	}
 }
 
-export async function fetchWeatherOneCall(
+export async function getWeatherOneCall(
 	lat: number,
 	lon: number
 ): Promise<OneCallWeatherData> {
@@ -54,7 +118,7 @@ export async function fetchWeatherOneCall(
 	}
 }
 
-export async function fetchAirPollution(
+export async function getAirPollution(
 	lat: number,
 	lon: number
 ): Promise<AirPollution> {
@@ -66,6 +130,19 @@ export async function fetchAirPollution(
 			{ timeout: 10000 }
 		);
 		return res.data;
+	} catch (error: any) {
+		throw error;
+	}
+}
+
+export async function getWeatherData(
+	lat: number,
+	lon: number
+): Promise<{ weather: OneCallWeatherData; airPollution: AirPollution }> {
+	try {
+		const weather = await getWeatherOneCall(lat, lon);
+		const airPollution = await getAirPollution(lat, lon);
+		return { weather: weather, airPollution: airPollution };
 	} catch (error: any) {
 		throw error;
 	}
