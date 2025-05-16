3752bf9cac3709f4f68e174b3abaa36869e93f8c
Jacek Gizinski <jacektrg+github@gmail.com>
2025-05-16 17:19:37 +0200

    Updating props. Adding vendor prefixes. Updating styling.

--- src/components/Extra.vue
+++ src/components/Extra.vue
@@ -15,7 +15,7 @@ import Tooltip from "./Tooltip.vue";
 const props = defineProps<{
 	weather: OneCallWeatherData | null;
 	pollution: AirPollution | null;
-	unitsSystem: 'metric' | 'imperial';
+	unitsSystem: 'metric' | 'imperial' | undefined;
 }>();
 </script>
 
@@ -195,14 +195,12 @@ const props = defineProps<{
 						transform="translate(0 0)" />
 					<polygon points="25 20 23.25 20 21 29.031 18.792 20 17 20 19.5 30 22.5 30 25 20" />
 					<rect x="15" y="2" width="2" height="5" />
-					<rect x="21.6675" y="6.8536" width="4.958" height="1.9998"
-						transform="translate(1.5191 19.3744) rotate(-45)" />
+					<rect x="21.6675" y="6.8536" width="4.958" height="1.9998" transform="translate(1.5191 19.3744) rotate(-45)" />
 					<rect x="25" y="15" width="5" height="2" />
 					<rect x="2" y="15" width="5" height="2" />
 					<rect x="6.8536" y="5.3745" width="1.9998" height="4.958" transform="translate(-3.253 7.8535) rotate(-45)" />
 					<path d="M22,17H20V16a4,4,0,0,0-8,0v1H10V16a6,6,0,0,1,12,0Z" transform="translate(0 0)" />
-					<rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" fill="none"
-						width="32" height="32" />
+					<rect fill="none" width="32" height="32" />
 				</svg>
 			</div>
 			<span>{{ ultravioletIndexToDescription(weather.current.uvi) }}</span>
@@ -247,6 +245,8 @@ const props = defineProps<{
 	grid-template-columns: repeat(3, minmax(68px, 1fr));
 	grid-gap: 1rem;
 	justify-content: space-between;
+	height: -webkit-fit-content;
+	height: -moz-fit-content;
 	height: fit-content;
 
 	>div {
@@ -352,12 +352,12 @@ const props = defineProps<{
 	margin-bottom: 0.25rem;
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
-	border: 1px solid var(--color-text);
-	gap: 1px;
-	background-color: var(--color-text);
+	border: 2px solid var(--color-border);
+	gap: 2px;
+	background-color: var(--color-border);
 
 	>div {
-		padding: 0.25rem 0.5rem;
+		padding: 0.125rem 0.25rem;
 		text-transform: none;
 		background-color: var(--color-background-soft);
 	}
@@ -390,7 +390,7 @@ const props = defineProps<{
 
 @media (min-width: 940px) {
 	.extra-widgets {
-		grid-template-columns: repeat(5, minmax(68px, 1fr));
+		grid-template-columns: repeat(4, minmax(68px, 1fr));
 	}
 }
 </style>
