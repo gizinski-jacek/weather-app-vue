fd6ff700ac14f753760607137570a5ac511a19ac
Jacek Gizinski <jacektrg+github@gmail.com>
2025-05-16 16:39:02 +0200

    Updating props types.
    Adding vendor prefixes.

--- src/components/Extra.vue
+++ src/components/Extra.vue
@@ -15,7 +15,7 @@ import Tooltip from "./Tooltip.vue";
 const props = defineProps<{
 	weather: OneCallWeatherData | null;
 	pollution: AirPollution | null;
-	unitsSystem: 'metric' | 'imperial';
+	unitsSystem: 'metric' | 'imperial' | undefined;
 }>();
 </script>
 
@@ -195,8 +195,7 @@ const props = defineProps<{
 						transform="translate(0 0)" />
 					<polygon points="25 20 23.25 20 21 29.031 18.792 20 17 20 19.5 30 22.5 30 25 20" />
 					<rect x="15" y="2" width="2" height="5" />
-					<rect x="21.6675" y="6.8536" width="4.958" height="1.9998"
-						transform="translate(1.5191 19.3744) rotate(-45)" />
+					<rect x="21.6675" y="6.8536" width="4.958" height="1.9998" transform="translate(1.5191 19.3744) rotate(-45)" />
 					<rect x="25" y="15" width="5" height="2" />
 					<rect x="2" y="15" width="5" height="2" />
 					<rect x="6.8536" y="5.3745" width="1.9998" height="4.958" transform="translate(-3.253 7.8535) rotate(-45)" />
@@ -246,6 +245,8 @@ const props = defineProps<{
 	grid-template-columns: repeat(3, minmax(68px, 1fr));
 	grid-gap: 1rem;
 	justify-content: space-between;
+	height: -webkit-fit-content;
+	height: -moz-fit-content;
 	height: fit-content;
 
 	>div {
